5ff4c8d18d0556002840a1de29a03fd6dcbde3a1
Pollák Tamás <Pollak.Tamas@apertus.uni-nke.hu>
2017-06-19 10:59:44 +0200

    Optional

---
+++ src/main/java/com/pollaktamas/functionalprogramming/optional/Main.java
@@ -0,0 +1,38 @@
+package com.pollaktamas.functionalprogramming.optional;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Optional<Double> opt0 = average(90, 100);
+        if (opt0.isPresent()) {
+            System.out.println(opt0.get());
+        }
+
+        // Empty optional
+        System.out.println(average());
+
+        // ifPresent
+        Optional<Double> opt1 = average(90, 100);
+        opt1.ifPresent(System.out::println);
+
+        // Other methods
+        Optional<Double> opt2 = average();
+        System.out.println(opt2.orElse(Double.NaN));
+        System.out.println(opt2.orElseGet(() -> Math.random()));
+        System.out.println(opt2.orElseThrow(() -> new IllegalStateException()));
+    }
+
+    public static Optional<Double> average(int... scores) {
+        if (scores.length == 0) {
+            return Optional.empty();
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return Optional.of((double) sum / scores.length);
+    }
+
+}
