fe7616d1be6f1cb30b09a275e8f798f1a75b5ed3
Pollák Tamás <Pollak.Tamas@apertus.uni-nke.hu>
2017-08-18 16:32:38 +0200

    Enum példák

--- src/main/java/com/pollaktamas/enums/Main.java
+++ src/main/java/com/pollaktamas/enums/Main.java
@@ -6,12 +6,27 @@ public class Main {
         System.out.println(fall);
         System.out.println(fall == Season.FALL);
 
-        for (Season season: Season.values()){
+        for (Season season : Season.values()) {
             System.out.println(season);
         }
 
         Season fromString = Season.valueOf("FALL");
         fromString.printAbbreviation();
         fromString.printExpectedVisitors();
+
+        Planet earth = Planet.EARTH;
+        System.out.println(earth.surfaceGravity());
+
+        printSurfaceWeightsOnAllPlanets(74);
+
+        System.out.println(Operation.PLUS.apply(6, 4));
+        System.out.println(Operation.TIMES.apply(5, 20));
+    }
+
+    private static void printSurfaceWeightsOnAllPlanets(double earthWeight) {
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+        for (Planet p : Planet.values()) {
+            System.out.printf("Weight on %s is %f%n", p, p.surfaceWeight(mass));
+        }
     }
 }
